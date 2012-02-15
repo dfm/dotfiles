65ec8a8efdf6979d90efb3ad56e5fb83fc5ea16a
Dan F-M <danfm@nyu.edu>
2012-02-15 11:51:17 -0500

    actually adding bootstrap and a few safeguards

--- .zshrc
+++ .zshrc
@@ -5,23 +5,30 @@ DISABLE_AUTO_UPDATE="true"
 plugins=(brew git github heroku osx pip python )
 source $ZSH/oh-my-zsh.sh
 
+# PATH
 # Default path
 PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin"
-
 # My bin
 PATH="${HOME}/bin:${PATH}"
-
 # Python scripts
 PATH="/usr/local/share/python:${PATH}"
-
 # LaTeX
 PATH="/usr/local/texlive/2011/bin/x86_64-darwin:${PATH}"
-
 export PATH
+
+# Aliases
+alias j='z'
+alias c='clear'
+
+# Customizations
+export GREP_OPTIONS='--color=auto'
 
 # Enable z
 . ${HOME}/lib/z/z.sh
 function precmd () {
     _z --add "$(pwd -P)"
 }
+
+# Finally, local settings
+if [[ -s $HOME/.zshrc_local ]] ; then source $HOME/.zshrc_local ; fi
 
---
+++ bootstrap.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Warning: Don't forget the SSH keys first!
+
+# Get the dotfiles directory (based on where this script is running from)
+DOTDIR=$HOME/lib/dotfiles
+if [ `pwd` != $DOTDIR ]; then
+    echo "This must be run from $HOME/lib/dotfiles"
+    exit 1;
+fi
+
+mkdir -p $HOME/lib
+
+# # Setup .vim
+DOTVIM=$HOME/lib/dotvim
+if [ ! -d $DOTVIM ]; then
+    git clone git@github.com:dfm/.vim.git $DOTVIM
+    (cd $DOTVIM; git submodule update --init)
+    ln -Fs $DOTVIM $HOME/.vim
+    ln -Fs $DOTVIM/vimrc $HOME/.vimrc
+else
+    echo "dotvim is already installed."
+fi
+
+# Setup oh-my-zsh
+if [ ! -d $HOME/lib/oh-my-zsh ]; then
+    git clone https://github.com/robbyrussell/oh-my-zsh.git $HOME/lib/oh-my-zsh
+else
+    echo "oh-my-zsh is already installed."
+fi
+
+# # Setup z
+if [ ! -d $HOME/lib/z ]; then
+    git clone https://github.com/rupa/z.git $HOME/lib/z
+else
+    echo "z is already installed."
+fi
+
+# Links
+ln -Fs $DOTDIR/.zshrc     $HOME/.zshrc
+ln -Fs $DOTDIR/.gitconfig $HOME/.gitconfig
+
