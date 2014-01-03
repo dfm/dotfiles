9cb38f54508a5d5b10e1116cff7a12d552306519
Dan F-M <danfm@nyu.edu>
2014-01-03 10:25:50 -0500

    mutt setup refactoring

--- mutt/offlineimap.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-import keyring
-
-
-def get_keychain_pass(account=None, server=None):
-    return keyring.get_password(server, account)
--- mutt/start_offlineimap.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-launchctl unload -w ~/Library/LaunchAgents/ca.danfm.offlineimap.plist
-cp offlineimap.plist ~/Library/LaunchAgents/ca.danfm.offlineimap.plist
-launchctl load -w ~/Library/LaunchAgents/ca.danfm.offlineimap.plist
